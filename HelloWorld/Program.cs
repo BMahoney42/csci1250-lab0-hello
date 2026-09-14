fe8b6c6a25684d4ab65932a925d9fdf2f23dc7c0
Brayton Moloney <moloneyb12@gmail.com>
2026-09-14 11:12:45 -0400

    Add header comment and introduction lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+*Name: Brayton MOloney
+*Course: CSCI 1250, Section 001
+*Assignment: Lab 0, Hello World
+*DATE: September 9, 2026
+*Description: Prints a greeting and short introduction to the console.
+*/
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Brayton Moloney.");
+Console.WriteLine("This is my First Program at ETSU.");
